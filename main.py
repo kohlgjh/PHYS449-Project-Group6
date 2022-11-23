c5ac32cfaf1b6675c74564d9cd63099bb8be1f92
Delaney Dyment <dldyment@uwaterloo.ca>
2022-11-22 22:32:27 -0500

    Add param support for cases

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import torch.nn as nn
 def parse_args():
     '''Parses command line input for arguments'''
     parser = argparse.ArgumentParser()
-    parser.add_argument("--case", type=int, help="integer 1-8 of what feature set to use")
+    parser.add_argument("--case", help="integer 1-8 of what feature set to use")
     parser.add_argument('-v', "--verbose", default="True", help="True/False for high/low verbosity")
     parser.add_argument("--seed", default=1234, help="random seed")
     parser.add_argument("--params", default='input/params.json', help="JSON filepath containing hyperparameters")
@@ -19,13 +19,20 @@ def parse_args():
 def main(args):
     '''Main code block'''
     verbose = True if args.verbose == "True" else False
-    case = int(args.case)
+    case = str(args.case)
     
     np.random.seed(args.seed)
     
     # hyperparameters from json file
     with open(args.params) as paramfile:
         param = json.load(paramfile)
+        
+    case_param = param[case]
+    input_size = case_param['input_size']
+    hidden_size = case_param['hidden_size']
+    epochs = case_param['epochs']
+    learning_rate = case_param['learning_rate']
+    momentum = case_param['momentum']
 
     # generate datasets
     train_input, train_target, test_input, test_target = generate_train_and_test(case)
