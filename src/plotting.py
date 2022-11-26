bec9d9205adc66f94b0ff3a14d77d6c67d85cdb5
Delaney Dyment <dldyment@uwaterloo.ca>
2022-11-26 17:10:11 -0500

    Combine RWNN and vanilla plots

--- src/plotting.py
+++ src/plotting.py
@@ -5,28 +5,21 @@ import matplotlib.pyplot as plt
 def plot_train_test(rwnn_obj_vals, rwnn_cross_vals, vanilla_obj_vals, vanilla_cross_vals, res_path):
     '''Plots the training and testing results'''
     
-    assert len(rwnn_obj_vals) == len(rwnn_cross_vals), 'Length mismatch between RWNN loss curves'
-    num_epochs = len(rwnn_obj_vals[-1])
-
-    # Plot saved in results folder
-    plt.plot(range(num_epochs), rwnn_obj_vals[-1], label= "Training", color="blue")
-    plt.plot(range(num_epochs), rwnn_cross_vals[-1], label= "Testing", color= "green")
-    plt.legend()
-    plt.title('RWNN Model Loss for Final Iteration')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.savefig(res_path + 'rwnn_loss_fig.jpeg')
-    plt.close()
+    # change format of RWNN results to plot sequential iterations
+    rwnn_obj_flat = [item for sublist in rwnn_obj_vals for item in sublist]
+    rwnn_cross_flat =  [item for sublist in rwnn_cross_vals for item in sublist]
     
-    assert len(vanilla_obj_vals) == len(vanilla_cross_vals), 'Length mismatch between vanilla loss curves'
-    num_epochs = len(vanilla_obj_vals)
+    assert len(rwnn_obj_flat) == len(rwnn_cross_flat), 'Length mismatch between loss curves'
+    num_epochs = len(rwnn_obj_flat)
 
     # Plot saved in results folder
-    plt.plot(range(num_epochs), vanilla_obj_vals, label= "Training", color="blue")
-    plt.plot(range(num_epochs), vanilla_cross_vals, label= "Testing", color= "green")
+    plt.plot(range(num_epochs), rwnn_obj_flat, label= "RWNN Training", color="blue")
+    plt.plot(range(num_epochs), rwnn_cross_flat, label= " RWNN Testing", color= "green")
+    plt.plot(range(num_epochs), vanilla_obj_vals, label= "Vanilla Training", color="red")
+    plt.plot(range(num_epochs), vanilla_cross_vals, label= " Vanilla Testing", color= "darkviolet")
     plt.legend()
-    plt.title('Vanilla Model Loss')
+    plt.title('Loss of RWNN and Vanilla Models')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
-    plt.savefig(res_path + 'van_loss_fig.pdf')
+    plt.savefig(res_path + 'loss_fig.pdf')
     plt.close()
